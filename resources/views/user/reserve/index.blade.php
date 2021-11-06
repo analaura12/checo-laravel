97df1d5ad23d967b952301988c725679fc491eaf
brubandrade <61695979+brubandrade@users.noreply.github.com>
2021-11-06 07:38:18 -0300

    Add files via upload

--- resources/views/user/reserve/index.blade.php
+++ resources/views/user/reserve/index.blade.php
@@ -37,26 +37,74 @@
                                                 </tr>
                                                 </thead>
                                                 <tbody>
-                                                    <tr>
-                                                        <td>2</td>
-                                                        <td>Outback</td>
-                                                        <td>03/11/21</td>
-                                                        <td>20:30</td>
-                                                        <td>3</td>
-                                                        <td>Porção</td>
-                                                        <td>R$50</td>
-                                                        <td>
-                                                            <span class="green">
-                                                                <i class="fas fa-check-circle" title="Ativo"></i>
-                                                            </span>
-                                                        </td>
-                                                        <td>
-                                                            <a  class="btn btn-danger delete-confirm" title="Cancelar"
-                                                                data-original-title="Cancelar" href="">
-                                                                <i class="fas fa-times"></i>
-                                                            </a>
-                                                        </td>
-                                                    </tr>
+                                                    @if(isset($reserves) && !empty($reserves))
+                                                        @foreach ($reserves as $res)
+                                                            <tr>
+                                                                <td>{{ $res->id }}</td>
+                                                                <td>
+                                                                    @foreach ($establishments as $est)
+                                                                        @if($res->establishment_id == $est->id)
+                                                                            {{ $est->name }}
+                                                                        @endif
+                                                                    @endforeach
+                                                                </td>
+                                                                <td>{{ $res->date }}</td> 
+                                                                <td>{{ $res->hour }}</td>
+                                                                <td>
+                                                                    @foreach ($tables as $tab)
+                                                                        @if($res->table_id == $tab->id)
+                                                                            {{ $tab->number }}
+                                                                        @endif
+                                                                    @endforeach
+                                                                </td>
+                                                                <td> 
+                                                                    @foreach ($products as $pro)
+                                                                        @if($res->product_id == $pro->id)
+                                                                            {{ $pro->name }}
+                                                                        @endif 
+                                                                    @endforeach
+                                                                </td>
+                                                                <td>
+                                                                    @foreach ($products as $pro)
+                                                                        @if($res->product_id == $pro->id)
+                                                                            R${{ $pro->price }}
+                                                                        @endif
+                                                                    @endforeach
+                                                                </td>
+                                                                <td>
+                                                                    @switch($res->status_id)
+                                                                        @case(1)
+                                                                            <span class="grey"><i class="fas fa-clock" title="Pendente"></i></span>
+                                                                            @break
+                                                                        @case(2)
+                                                                            <span class="green"><i class="fas fa-check-circle" title="Aprovado"></i></span>
+                                                                            @break
+                                                                        @case(3)
+                                                                            <span class="red"><i class="far fa-times-circle" title="Desaprovado"></i></span>
+                                                                            @break
+                                                                        @case(4)
+                                                                            <span class="grey"><i class="fas fa-calendar-check" title="Finalizada"></i></span>
+                                                                            @break
+                                                                        @case(5)
+                                                                            <span class="red"><i class="fas fa-calendar-times" title="Cancelada"></i></span>
+                                                                            @break
+                                                                        @default
+                                                                        <span class="green"><i class="fas fa-calendar-day" title="Em andamento"></i></span>
+                                                                    @endswitch
+                                                                </td>
+                                                                <td>
+                                                                    @if($res->status_id == 1 || $res->status_id == 2 || $res->status_id == 6)
+                                                                        <a  class="btn btn-danger delete-confirm" title="Cancelar"
+                                                                            data-original-title="Cancelar" href="{{route('reserve.update', $res->id)}}">
+                                                                            <i class="fas fa-times"></i>
+                                                                        </a>
+                                                                    @endif
+                                                                </td>
+                                                            </tr>
+                                                        @endforeach
+                                                    @else
+                                                        <h5>Não há reservas.</h5>
+                                                    @endif
                                                 </tbody>
                                             </table>
                                           <!--  @if(isset($tables) && !empty($tables))
